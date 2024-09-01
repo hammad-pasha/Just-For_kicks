5ea55c46890788930d425ec768315aa2daf95a40
ameenilyas <ameenilyas78@gmail.com>
2024-09-01 19:48:30 +0500

    added mac in create product

--- frontend/src/components/Shop/CreateProduct.jsx
+++ frontend/src/components/Shop/CreateProduct.jsx
@@ -195,6 +195,7 @@ const CreateProduct = () => {
           <input
             type="number"
             name="price"
+            max={originalPrice}
             value={discountPrice}
             className="mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
             onChange={(e) => setDiscountPrice(e.target.value)}
