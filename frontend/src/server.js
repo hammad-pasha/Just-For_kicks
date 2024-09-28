a7b7673d360ae100591e27e5ba047e55f76df110
hammad <74132151+hammad-pasha@users.noreply.github.com>
2024-09-28 14:45:21 +0500

    Update server.js url

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,2 @@
-export const server = "http://localhost:8000/api/v2";
-export const backend_url = "http://localhost:8000";
+export const server = "https://just-for-kicks-u4le.vercel.app/api/v2";
+export const backend_url = "https://just-for-kicks-u4le.vercel.app";
