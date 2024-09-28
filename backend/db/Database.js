d3b503c2e4c9fde6754704e9e4152484e559d9fd
hammad <74132151+hammad-pasha@users.noreply.github.com>
2024-09-28 16:52:04 +0500

    fsdg

--- backend/db/Database.js
+++ backend/db/Database.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDatabase = () => {
+  console.log("Mongodb uri: ", process.env.MONGODB_URI);
   mongoose
     .connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
