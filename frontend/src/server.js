e4bf43f74a463cd2c2f0710530027aedfb16ab30
hammad <74132151+hammad-pasha@users.noreply.github.com>
2024-09-28 15:25:40 +0500

    asdf

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,2 @@
-export const server = "https://just-for-kicks-elj9.vercel.app/api/v2";
-export const backend_url = "https://just-for-kicks-elj9.vercel.app";
+export const server = "https://just-for-kicks-u4le.vercel.app/api/v2";
+export const backend_url = "https://just-for-kicks-u4le.vercel.app";
